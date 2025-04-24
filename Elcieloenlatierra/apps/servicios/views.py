fd02e3e440f20011584f51f32dc67f7ba099a7be
L2Chance <Estudiandoyavanzando@gmail.com>
2025-04-24 11:06:45 -0300

    backend del formulario de servicios modificado (alerts)

--- Elcieloenlatierra/apps/servicios/views.py
+++ Elcieloenlatierra/apps/servicios/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from .forms import ServicioForm
 
 def crear_servicio(request):
+    exito = False 
     if request.method == 'POST':
         form = ServicioForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/home') 
+            exito = True  
+            return redirect('/home')  
     else:
         form = ServicioForm()
-    return render(request, 'servicios/formularios/nuevo_servicio.html', {'form': form})
+    
+    return render(request, 'servicios/formularios/nuevo_servicio.html', {'form': form, 'exito': exito})
