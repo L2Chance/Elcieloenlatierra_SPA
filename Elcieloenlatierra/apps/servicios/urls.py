d925d93f410720772f653f5f2d4e82052e454474
L2Chance <Estudiandoyavanzando@gmail.com>
2025-04-27 20:12:51 -0300

    agregadas funciones de eliminar y modificar servicios

--- Elcieloenlatierra/apps/servicios/urls.py
+++ Elcieloenlatierra/apps/servicios/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('nuevo/', views.crear_servicio, name='crear_servicio'),
     path('listado/', views.listar_servicios, name='listar_servicios'),
+    path('modificar/<int:pk>/', views.modificar_servicio, name='modificar_servicio'),
+    path('eliminar/<int:pk>/', views.eliminar_servicio, name='modificar_servicio')
 ]
--- Elcieloenlatierra/apps/servicios/views.py
+++ Elcieloenlatierra/apps/servicios/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .forms import ServicioForm
 from .models import Servicio
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
+#FUNCIÓN PARA LISTAR SERVICIO
+
+def listar_servicios(request):
+    servicios = Servicio.objects.all()
+    form_vacio = ServicioForm() 
+    context = {'servicios': servicios, 'form': form_vacio}
+    return render(request, 'servicios/servicios.html', context)
+
+
+#FUNCIÓN PARA CREAR SERVICIO
 
 @login_required
 def crear_servicio(request):
@@ -17,6 +30,30 @@ def crear_servicio(request):
     
     return render(request, 'servicios/formularios/nuevo_servicio.html', {'form': form, 'exito': exito})
 
-def listar_servicios(request):
-    servicios = Servicio.objects.all()
-    return render(request, 'servicios/servicios.html', {'servicios': servicios})
+
+#FUNCIÓN PARA MODIFICAR SERVICIO
+
+def modificar_servicio(request, pk):
+    servicio = get_object_or_404(Servicio, pk=pk)
+    exito = False
+
+    if request.method == 'POST':
+        form = ServicioForm(request.POST, request.FILES, instance=servicio)
+        if form.is_valid():
+            form.save()
+            exito = True
+    else:
+        form = ServicioForm(instance=servicio)
+
+    return render(request, 'servicios/formularios/modificar_servicio.html', {'form': form, 'exito': exito, 'servicio': servicio})
+
+#FUNCIÓN PARA ELIMINAR SERVICIO
+
+def eliminar_servicio(request, pk):
+    servicio = get_object_or_404(Servicio, pk=pk)
+
+    if request.method == 'POST':
+        servicio.delete()
+        return redirect('listar_servicios') 
+    else:
+        return render(request, 'servicios/formularios/eliminar_servicio.html', {'servicio': servicio})
