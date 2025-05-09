8c3fe7ed01a7a567c988635db36f8716d8218cf6
L2Chance <Estudiandoyavanzando@gmail.com>
2025-05-09 12:13:09 -0300

    Actualizado para deploy

--- Elcieloenlatierra/Elcieloenlatierra/settings.py
+++ Elcieloenlatierra/Elcieloenlatierra/settings.py
@@ -123,8 +123,5 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
